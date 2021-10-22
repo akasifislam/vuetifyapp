0e548a0785c40e8997d1899760a3999697f554dc
akasifislam <aseasifislam@gmail.com>
2021-10-22 20:54:24 +0600

    Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="my-15">
+      <v-row justify="center">
+          <v-col lg="12" class="text-center">
+              <v-subheader class="text-h4 justify-center">Contact Us</v-subheader>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis ut dolore laudantium nulla eveniet alias minus explicabo ea labore vitae quod cupiditate et doloremque repellat quam, iure expedita consequatur quo</p>
+          </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
