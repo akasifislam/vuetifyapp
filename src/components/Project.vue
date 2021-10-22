8f6896e7f977caec9b114fe73ff58de9fd147ba6
akasifislam <aseasifislam@gmail.com>
2021-10-22 17:42:23 +0600

    Project component crerated

---
+++ src/components/Project.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-continaer>
+      <v-row>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. At commodi maxime vel deleniti harum in soluta quam odio sint, nesciunt eveniet nisi impedit dignissimos sit possimus iure culpa tenetur blanditiis.</h1>
+      </v-row>
+  </v-continaer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
